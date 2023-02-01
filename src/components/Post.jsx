3b3f61d7e23064430243ea2719598dc7b704a9c2
GuilheermeMarques <guiferreira_94@hotmail.com>
2023-01-31 23:39:05 -0300

    using the avatar component in the post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,9 +7,8 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img 
-                        className={styles.avatar}
-                        src="https://github.com/GuilheermeMarques.png" 
+                    <Avatar 
+                        src="https://github.com/DavidSouza22.png" 
                     />
                     <div className={styles.authorInfo}>
                         <strong>Guilherme Marques</strong>
